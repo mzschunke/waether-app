fd074c2a59c80f1306a2259bb90137e13367e5b5
Moritz Zschunke <moritz_z17@web.de>
2023-04-13 12:59:26 +0200

    implemented AddActivityFunction - but desperate about boolean

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -2,22 +2,22 @@ import react from 'react';
 import "./Form.css";
 
 
-export default function Form () {
+export default function Form ({onAddActivity}) {
     
 
     function handleSubmit(event) {
         event.preventDefault();
-        const formData = new FormData(event.target);
-        const data = Object.fromEntries(formData);
+       const formData = new FormData(event.target);
+       const data = Object.fromEntries(formData);
         console.log(data);
-        //onAddActivity(data);
+        onAddActivity(data); 
         event.target.reset();
         event.target.name.focus();
-
-       /* const form = event.target;
+    /*   const form = event.target;
         const name = form.activity.value;
         const goodWeather = form.checkbox.checked;  
-        console.log(name, goodWeather); */
+        console.log(name, goodWeather); 
+        */
     }
 
     return (
@@ -28,7 +28,7 @@ export default function Form () {
       </label>
       <input id="name" name="name" type="text" />
       <label htmlFor='isForGoodWeather'>Good-weather activity:</label> 
-      <input type="checkbox" name="isForGoodWeather"/> 
+      <input type="checkbox" name="isForGoodWeather" id="checkbox"/> 
         <button type="submit">Submit</button>
     </form>
     )}
